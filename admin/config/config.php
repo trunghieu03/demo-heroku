7c630cca3ef6465f1a0f96ffbc1b07b6917fc0d3
TrungHieu <114654825+trunghieu03@users.noreply.github.com>
2024-05-15 00:46:22 +0700

    Update config.php

--- admin/config/config.php
+++ admin/config/config.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("us-cluster-east-01.k8s.cleardb.net","bdb349a88d3d26","6dd0c0f7","heroku_1bbd367bec98762");
+$mysqli = new mysqli("us-cluster-east-01.k8s.cleardb.net","bdb349a88d3d26","6dd0c0f7","shopgiay");
 
 // Check connection
 if ($mysqli -> connect_errno) {
